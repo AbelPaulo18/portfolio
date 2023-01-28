b08729f1ec78b2f323db3b0fb80073847fbb27e8
AbelPaulo18 <abelpaulo.2218@gmail.com>
2023-01-28 13:16:31 +0100

    Header component creation

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export const Header = (props: Props) => {
   return (
-    <header>
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-4xl mx-auto z-20 xl:items-center ">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.3,
+        }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://www.linkedin.com/in/abel-paulo-5394311bb/"
           bgColor="transparent"
@@ -17,18 +33,32 @@ export const Header = (props: Props) => {
           bgColor="transparent"
           fgColor="gray"
         />
-      </div>
-      <div>
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+      >
         <SocialIcon
           className="cursor-pointer"
           bgColor="transparent"
           fgColor="gray"
           network="email"
         />
-        <p className="uppercase font-semibold text-stone-500 hidden md:inline-flex ">
+        <p className="uppercase font-semibold text-stone-500 hidden md:inline-flex text-sm ">
           Contact me
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 };
